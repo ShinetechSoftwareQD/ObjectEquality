89903e69bd32800cba1d28708d55263ebc7fca9d
Lamond Lu <lunan@shintechchina.com>
2018-12-26 06:50:37 +0800

    Add object extension

---
+++ ObjectEqual/ObjectExtension.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace ObjectEquality
+{
+    public static class ObjectExtension
+    {
+        public static bool IsEqual(this object source, object target)
+        {
+            if (source.GetType() != target.GetType())
+            {
+                return false;
+            }
+
+            if (source == null && target == null)
+            {
+                return true;
+            }
+            else if (source == null && target != null)
+            {
+                return false;
+            }
+            else if (source != null && target == null)
+            {
+                return false;
+            }
+
+            var equality = EqualityCollection.Equalities.First(p => p.MatchCondition(source));
+
+            return equality.IsEqual(source, target);
+        }
+    }
+}
