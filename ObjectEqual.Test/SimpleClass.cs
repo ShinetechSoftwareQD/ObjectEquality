06850e2ac3ed6f6974330054d0378f2f9df529cb
Lamond Lu <lunan@shintechchina.com>
2021-08-22 09:59:23 +0800

    Clean up the code

--- ObjectEqual.Test/SimpleClass.cs
+++ ObjectEqual.Test/SimpleClass.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace ObjectEquality.Test
+﻿namespace ObjectEquality.Test
 {
     public class SimpleClass
     {
--- ObjectEqual/Options/ArrayEqualityMode.cs
+++ ObjectEqual/Options/ArrayEqualityMode.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace ObjectEquality
+﻿namespace ObjectEquality
 {
     public enum ArrayEqualityMode
     {
